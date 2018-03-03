0a5b9783b87481f8a164ece98705d770ea52602d
agoddijn <alex.goddijn@gmail.com>
2018-03-02 20:00:44 -0800

    heroku deploy

--- mea/wsgi.py
+++ mea/wsgi.py
@@ -9,7 +9,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mea.settings")
 
 application = get_wsgi_application()
 
