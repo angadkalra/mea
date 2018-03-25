f63f0d1fd132a761e60c764ee883bfeeeacf97c6
Angad Kalra <o1c9@ugrad.cs.ubc.ca>
2018-03-25 16:45:43 -0700

    fixed finding curators.

--- mea/recommend.py
+++ mea/recommend.py
@@ -12,19 +12,21 @@ class SimilarUsers():
 	def find(user):
 		# number of users
 		n = Profile.objects.count()
+		print("Number of users: ", n)
 
 		# Create 2d array of profile id's and similarity scores
-		scores = np.zeros([n,2], dtype=np.float)  
+		scores = np.zeros((n,2))  
 
 		# From user, get movies they selected. 
 		user_movies = np.array(list(map(lambda x: x.id, user.profile.movies.all())))
 
 		# get list of current users
+		i = 0
 		for profile in Profile.objects.all():
-			index = profile.id
 
 			if profile.id == user.id:
-				scores[index-1, 0] = profile.id
+				scores[i, 0] = profile.id
+				i += 1
 				continue
 
 			movies = np.array(list(map(lambda x: x.id, profile.movies.all())))
@@ -33,21 +35,24 @@ class SimilarUsers():
 			union = np.union1d(user_movies, movies)
 			jaccard = np.float(intersection.size/union.size)
 
-			scores[index-1, 0] = profile.id
-			scores[index-1, 1] = jaccard
+			scores[i, 0] = profile.id
+			scores[i, 1] = jaccard
+
+			i += 1
 
 		# Sort scores by decreasing order of similarity
 		scores = scores[scores[:,1].argsort()[::-1]]
 		
 		# Return top 10 matches
-		top_matches = scores[1:3,0].astype(np.int).tolist()
+		top_matches = scores[1:11,0].astype(np.int).tolist()
 
 		data = []
 		for p in top_matches:
 			obj = {}
 			profile = Profile.objects.get(id = p)
 			
-			obj['bio'] = profile.bio
+			obj['firstName'] = profile.user.first_name
+			obj['lastName'] = profile.user.last_name
 			obj['movies'] = list(map(lambda x: x.imdbId, profile.movies.all()))
 
 			data.append(obj)
